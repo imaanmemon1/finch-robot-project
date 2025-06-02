395ad320ef6e6e422f56b1ee01472e4f27af912c
imaanmemon1 <memonimaan880@gmail.com>
2025-06-02 09:35:12 -0700

    commit

--- src/main/java/com/ImJi/FinalFinch.java
+++ src/main/java/com/ImJi/FinalFinch.java
@@ -1,5 +1,21 @@
-public class FinalFinch {
-    public static void main(String[] args) {
+package com.ImJi;
 
+import com.birdbrain.Finch;
+
+public class FinchDemo {
+    public static void main(String[] args) {
+        Finch myFinch = new Finch();
+
+        myFinch.playNote(60,0.5);
+
+        for (int i = 0; i < 10; i++) {
+            myFinch.setBeak(100, 100, 100);
+            myFinch.pause(1);
+            myFinch.setBeak(0, 0, 0);
+            myFinch.pause(1);
+        }
+
+        myFinch.stopAll();
+        myFinch.disconnect();
     }
 }
