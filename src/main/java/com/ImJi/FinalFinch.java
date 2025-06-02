f982ffb97b3eedbd7a59bc8cc8a729cdf7cc1850
imaanmemon1 <memonimaan880@gmail.com>
2025-06-02 09:47:29 -0700

    Update FinalFinch.java

--- src/main/java/com/ImJi/FinalFinch.java
+++ src/main/java/com/ImJi/FinalFinch.java
@@ -1,5 +1,16 @@
+package com.ImJi;
+
+import com.birdbrain.Finch;
+
 public class FinalFinch {
     public static void main(String[] args) {
+        Finch myFinch = new Finch();
+
+        // Print a message on the micro:bit
+        myFinch.print("Dance!");
+
+        // Play an intro note
+        myFinch.playNote(60, 0.5);
 
     }
 }
