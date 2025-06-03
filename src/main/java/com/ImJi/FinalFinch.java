10d8b57754f56f4d5be840e088bc30eda3dbdb57
imaanmemon1 <memonimaan880@gmail.com>
2025-06-03 08:53:35 -0700

    Update FinalFinch.java

--- src/main/java/com/ImJi/FinalFinch.java
+++ src/main/java/com/ImJi/FinalFinch.java
@@ -6,16 +6,10 @@ public class FinalFinch {
     public static void main(String[] args) {
         Finch myFinch = new Finch();
 
-        myFinch.playNote(60,0.5);
+        // myFinch.print("Dance!");
 
-        for (int i = 0; i < 10; i++) {
-            myFinch.setBeak(100, 100, 100);
-            myFinch.pause(1);
-            myFinch.setBeak(0, 0, 0);
-            myFinch.pause(1);
-        }
+        // Play an intro note
+        myFinch.playNote(60, 0.5);
 
-        myFinch.stopAll();
-        myFinch.disconnect();
     }
 }
