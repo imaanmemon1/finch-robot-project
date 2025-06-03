6bdf94ae5cd61f0c28f648f69935abb8b518070d
imaanmemon1 <memonimaan880@gmail.com>
2025-06-03 09:13:20 -0700

    Update FinalFinch.java

--- src/main/java/com/ImJi/FinalFinch.java
+++ src/main/java/com/ImJi/FinalFinch.java
@@ -6,6 +6,16 @@ public class FinalFinch {
     public static void main(String[] args) {
         Finch bird = new Finch();
 
-        bird.setTail(1,100,0,50)
+
+
+        bird.setTail(1,100,0,50);
+
+
+
+
+
+
+        bird.stopAll();
+        bird.disconnect();
     }
 }
