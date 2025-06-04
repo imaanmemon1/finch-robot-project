a33e05e86e8dbe5f7be4f2092db94857982d4d00
imaanmemon1 <memonimaan880@gmail.com>
2025-06-04 08:38:32 -0700

    Commit

--- src/main/java/com/ImJi/FinalFinch.java
+++ src/main/java/com/ImJi/FinalFinch.java
@@ -6,16 +6,22 @@ public class FinalFinch {
     public static void main(String[] args) {
         Finch bird = new Finch();
 
+        bird.setTurn("L",40, 60);
 
+        bird.setMove("F", 12, 20);
+        bird.setMotors(40, 0);
+        bird.pause(2.4);
 
-        bird.setTail(1,100,0,50);
+        bird.setTurn("L", 182, 60);
 
-
-
-
-
+        bird.setMotors(40, 0);
+        bird.pause(2.4);
+        bird.setMotors(0,0); 
+        bird.pause(0.5);          
+        bird.setMove("F", 12, 20);
 
         bird.stopAll();
         bird.disconnect();
+
     }
 }
